244d12f3d4e2bc711ffd0a724d35f97d76c053df
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-09-23 20:26:03 +0600

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./About";
+import Cart from "./Cart";
+import Contact from "./Contact";
+import Error from "./Error";
+import Home from "./Home";
+import Products from "./Products";
+import SingleProduct from "./SingleProduct";
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/singleproduct/:id" element={<SingleProduct />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 };
 
 export default App;
---
+++ src/Error.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Error = () => {
+  return <div>This is a error page</div>;
+};
+
+export default Error;
