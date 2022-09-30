eee42f2d570ae72afd891763defaab4b94f9d0bd
Mahesh Biswas <maheshbiswas26@gmail.com>
2022-09-30 20:15:07 +0600

    contact_map

--- src/Contact.js
+++ src/Contact.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Contact = () => {
   const Wrapper = styled.section`
-    padding: 9rem 0 5rem 0;
+    padding: 3rem 0 5rem 0;
     text-align: center;
 
     .container {
@@ -33,7 +33,20 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <h2>Contact Page</h2>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117658.98533132086!2d89.46246113870049!3d22.845286583000416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff9071cb47152f%3A0xf04b212290718952!2sKhulna!5e0!3m2!1sen!2sbd!4v1664547169471!5m2!1sen!2sbd"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </Wrapper>
+  );
 };
 
 export default Contact;
